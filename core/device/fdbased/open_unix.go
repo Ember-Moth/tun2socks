914df9cd1e6c7caab8fbce5201409cf1f0d76e0b
Ember Moth <ember_moth@proton.me>
2025-02-12 10:57:21 +0800

    Update open_unix.go

--- core/device/fdbased/open_unix.go
+++ core/device/fdbased/open_unix.go
@@ -6,8 +6,8 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/xjasonlyu/tun2socks/core/device"
-	"github.com/xjasonlyu/tun2socks/core/device/iobased"
+	"github.com/Ember-Moth/tun2socks/core/device"
+	"github.com/Ember-Moth/tun2socks/core/device/iobased"
 )
 
 func open(fd int, mtu uint32, offset int) (device.Device, error) {
