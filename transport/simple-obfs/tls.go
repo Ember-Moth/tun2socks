e0c8331f81b8071a3c4f0427ad881ca51515d9db
Ember Moth <ember_moth@proton.me>
2025-02-12 09:02:18 +0800

    Update tls.go

--- transport/simple-obfs/tls.go
+++ transport/simple-obfs/tls.go
@@ -7,8 +7,8 @@ import (
 	"net"
 	"time"
 
-	"github.com/xjasonlyu/tun2socks/v2/buffer"
-	"github.com/xjasonlyu/tun2socks/v2/transport/internal/bufferpool"
+	"github.com/xjasonlyu/tun2socks/buffer"
+	"github.com/xjasonlyu/tun2socks/transport/internal/bufferpool"
 )
 
 const (
