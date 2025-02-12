ec82e2c08d7440db538643fd34cd751580dc3a5a
Ember Moth <ember_moth@proton.me>
2025-02-12 09:46:19 +0800

    Update android.go

--- mobile/android.go
+++ mobile/android.go
@@ -1,7 +1,6 @@
 package mobile
 
 import "github.com/xjasonlyu/tun2socks/log"
-import "go.uber.org/zap/zapcore"
 
 func init() {
     log.SetLogger(log.Must(log.NewLeveled(zapcore.Level(zapcore.InfoLevel))))
