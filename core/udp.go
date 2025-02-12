b5f8eed92b2e4b2312cfb976678ff37506e93818
Ember Moth <ember_moth@proton.me>
2025-02-12 10:59:27 +0800

    Update udp.go

--- core/udp.go
+++ core/udp.go
@@ -7,8 +7,8 @@ import (
 	"gvisor.dev/gvisor/pkg/tcpip/transport/udp"
 	"gvisor.dev/gvisor/pkg/waiter"
 
-	"github.com/xjasonlyu/tun2socks/core/adapter"
-	"github.com/xjasonlyu/tun2socks/core/option"
+	"github.com/Ember-Moth/tun2socks/core/adapter"
+	"github.com/Ember-Moth/tun2socks/core/option"
 )
 
 func withUDPHandler(handle func(adapter.UDPConn)) option.Option {
